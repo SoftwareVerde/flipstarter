61f19539d8eb5ea51b6e9438c4c0771f2e5a55e7
Jonathan Silverblood <gitlab@monsterbitar.se>
2020-03-10 19:42:38 +0200

    Only try to insert if data is already existing.

--- sql/database_init.sql
+++ sql/database_init.sql
@@ -1,21 +1,21 @@
 /* Add the initial campaign (from 2020-02-01 to 2020-03-21)*/
-INSERT INTO campaigns (campaign_id, starts, expires) VALUES (1, 1580508000, 1584748799);
+INSERT OR IGNORE INTO campaigns (campaign_id, starts, expires) VALUES (1, 1580508000, 1584748799);
 
 /* Add the recipient. */
-INSERT INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (1, 'https://twitter.com/monsterbitar', 'https://pbs.twimg.com/profile_images/490950516973637633/LkJHQeNM_400x400.png', 'Jonathan Silverblood', 'bitcoincash:qr4aadjrpu73d2wxwkxkcrt6gqxgu6a7usxfm96fst');
+INSERT OR IGNORE INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (1, 'https://twitter.com/monsterbitar', 'https://pbs.twimg.com/profile_images/490950516973637633/LkJHQeNM_400x400.png', 'Jonathan Silverblood', 'bitcoincash:qr4aadjrpu73d2wxwkxkcrt6gqxgu6a7usxfm96fst');
 
 /* Link recipient to the initial campaign. */
-INSERT INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (1, 2000000, 1, 1);
+INSERT OR IGNORE INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (1, 2000000, 1, 1);
 
 /**********************************************/
 
 /* Add the initial campaign (from 2020-02-01 to 2020-02-28)*/
-INSERT INTO campaigns (campaign_id, starts, expires) VALUES (2, 1580508000, 1582927199);
+INSERT OR IGNORE INTO campaigns (campaign_id, starts, expires) VALUES (2, 1580508000, 1582927199);
 
 /* Add both EatBCH projects as users. */
-INSERT INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (2, 'https://eatbch.org/venezuela/', 'https://eatbch.org/_assets/img/venezuela-hero-wide.jpg?0.0.11', 'EatBCH Venezuela',  'bitcoincash:ppwk8u8cg8cthr3jg0czzays6hsnysykes9amw07kv');
-INSERT INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (3, 'https://eatbch.org/south-sudan/', 'https://eatbch.org/_assets/img/south-sudan-hero-wide.jpg?0.0.11', 'EatBCH South Sudan', 'bitcoincash:qrsrvtc95gg8rrag7dge3jlnfs4j9pe0ugrmeml950');
+INSERT OR IGNORE INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (2, 'https://eatbch.org/venezuela/', 'https://eatbch.org/_assets/img/venezuela-hero-wide.jpg?0.0.11', 'EatBCH Venezuela',  'bitcoincash:ppwk8u8cg8cthr3jg0czzays6hsnysykes9amw07kv');
+INSERT OR IGNORE INTO users (user_id, user_url, user_image, user_alias, user_address) VALUES (3, 'https://eatbch.org/south-sudan/', 'https://eatbch.org/_assets/img/south-sudan-hero-wide.jpg?0.0.11', 'EatBCH South Sudan', 'bitcoincash:qrsrvtc95gg8rrag7dge3jlnfs4j9pe0ugrmeml950');
 
 /* Link both EathBCH projects as recipient to the initial campaign. */
-INSERT INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (2, 360000000, 2, 2);
-INSERT INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (3, 528000000, 3, 2);
+INSERT OR IGNORE INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (2, 360000000, 2, 2);
+INSERT OR IGNORE INTO recipients (recipient_id, recipient_satoshis, user_id, campaign_id) VALUES (3, 528000000, 3, 2);
