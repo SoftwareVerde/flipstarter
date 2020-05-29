b71ce38a28145bd50173c4f1053ad54d84234d95
Corentin Mercier <corentin@mercier.link>
2020-05-30 02:29:40 +0700

    Fetch port from env if available

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   //
   server: {
     // Which port the server should listen for requests on.
-    port: 3000,
+    port: process.env.PORT || 3000,
 
     // Where to store the servers database file(s).
     database: "./database.db",
