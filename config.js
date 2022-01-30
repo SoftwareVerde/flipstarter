ccf19272001d6ddd9ec86d5578d8cc777c7ae623
Salem Mohammed Shamakh <8745266-salemkode@users.noreply.gitlab.com>
2022-01-30 13:10:27 +0000

    Make flipstarter more safety

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ module.exports = {
     port: process.env.PORT || 3000,
 
     // Where to store the servers database file(s).
-    database: "./static/campaigns/database.db",
+    // Express serve-static ignores files whose names begin with a dot
+    // This avoids making the database available on a public URL.
+    database: "./static/campaigns/.database.db",
   },
 };
