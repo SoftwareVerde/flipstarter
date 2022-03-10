b0a05e804bab222d93c80240a3765e82e7bd69d0
Josh Green <josh@softwareverde.com>
2022-03-09 22:57:49 -0500

    Started wallet creation.

---
+++ web-wallet.js
@@ -0,0 +1,31 @@
+const libauth = require("@bitauth/libauth");
+
+window.libauth = libauth;
+
+class Wallet {
+    static async create() {
+        const crypto = await libauth.instantiateBIP32Crypto();
+
+        return new Wallet(crypto);
+    }
+
+    #_crypto = null;
+    #_privateKey = null;
+
+    constructor(crypto) {
+        this._crypto = crypto;
+
+        this._privateKey = libauth.generatePrivateKey(function() {
+            return window.crypto.getRandomValues(new Uint8Array(32))
+        });
+    }
+
+    getAddress() {
+        const publicKey = this._crypto.secp256k1.derivePublicKeyCompressed(this._privateKey);
+        const hash = this._crypto.ripemd160.hash(this._crypto.sha256.hash(publicKey));
+        return libauth.encodeCashAddress(libauth.CashAddressNetworkPrefix.mainnet, libauth.CashAddressVersionByte.P2PKH, hash);
+    }
+    
+}
+
+window.Wallet = Wallet;
